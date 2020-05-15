44a9382a444653907d0424f888a2313a63cb2997
jaga1208 <57870293+jaga1208@users.noreply.github.com>
2020-05-15 16:59:24 -0400

    Jenkins

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -11,18 +11,9 @@ yum install -y maven
 echo "Install git"
 yum install -y git
 
-echo "Install Docker engine"
-yum update -y
-yum install docker -y
-#sudo usermod -a -G docker jenkins
-#sudo service docker start
-sudo chkconfig docker on
-
 echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
-sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
-sudo service docker start
 sudo service jenkins start
