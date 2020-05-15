a63ebc3da9a94b41ebb15ec49c6efcfc1abad738
jaga1208 <57870293+jaga1208@users.noreply.github.com>
2020-05-15 17:23:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,34 @@
 resource "aws_instance" "myec2" {
-  ami = "ami-08cec7c429219e339"
+  ami = "ami-050d581a8c1d4a570"
   instance_type = "t2.micro"
   key_name = "my-key"
 }
-
-
-resource "aws_security_group" "sg_allow_ssh_jenkins" {
-  name        = "allow_ssh_jenkins"
-  description = "Allow SSH and Jenkins inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+resource "aws_security_group" "web_sg" {
+    name = "web_sg"
+    ingress {
+        to_port     = 22
+        from_port   = 22
+        protocol    = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+        to_port     = 22
+        from_port   = 22
+        protocol    = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+        to_port     = 8080
+        from_port   = 8080
+        protocol    = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+  } 
+  egress {
+        to_port     = 8080
+        from_port   = 8080
+        protocol    = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+  
