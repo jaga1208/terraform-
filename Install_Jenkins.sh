15bee4b6901ab996b3ed236a47482f15b14e2d38
jaga1208 <57870293+jaga1208@users.noreply.github.com>
2021-03-03 09:58:54 -0500

    Update Install_Jenkins.sh
    remove chkonfig

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -15,5 +15,4 @@ echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
-sudo chkconfig jenkins on
 sudo service jenkins start
